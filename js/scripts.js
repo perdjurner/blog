af10ae5400d9bbdd014cd973d76907d285f50d4f
Per Djurner <per.djurner@gmail.com>
2020-06-11 23:44:03 +0200

    Show post based on URL hash.

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,51 @@ function toHtml(md) {
 }
 
 function toDate(str) {
-  return new Intl.DateTimeFormat("en-US").format(Date.parse(str));
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  }).format(Date.parse(str));
+}
+
+function postsHtml(posts) {
+  let rv = "";
+  for (const post of posts) {
+    rv += `
+      <article> 
+        <div class="date">
+          ${toDate(post.closed_at)}
+        </div>
+        <div class="title">  
+          <h1>${post.title}</h1>
+        </div>
+        <div class="labels">
+          ${labelsHtml(post.labels)}
+        </div>
+        <div class="post">
+          ${toHtml(toEntities(post.body))}
+        </div>
+      </article>
+    `;
+  }
+  return rv;
+}
+
+function labelsHtml(labels) {
+  return labels.reduce((acc, cur, index) => {
+    const li = `<li>${cur.name}</li>`;
+    acc += index === 0 ? `<ul>${li}` : li;
+    return index === labels.length - 1 ? `${acc}</ul>` : acc;
+  }, "");
+}
+
+function footerHtml(user) {
+  return `
+    <a href="https://twitter.com/perdjurner">
+      <img src="/images/twitter.svg" alt="Twitter logo">
+      <p>${user.name}</p>
+    </a>
+  `;
 }
 
 function render(html) {
@@ -45,46 +89,23 @@ function render(html) {
 }
 
 (async () => {
+  // Load user and posts from the GitHub issues API.
   const user = await api("users/perdjurner");
-  let issues = await api("repos/perdjurner/blog/issues?state=closed");
-  issues = issues
-    .filter((elm) => elm.author_association === "OWNER")
-    .sort((a, b) => (a.closed_at < b.closed_at ? 1 : -1));
-  let articles = "";
-  for (const issue of issues) {
-    const labels = issue.labels.reduce((acc, cur, index) => {
-      const li = `<li>${cur.name}</li>`;
-      acc += index === 0 ? `<ul>${li}` : li;
-      return index === issue.labels.length - 1 ? `${acc}</ul>` : acc;
-    }, "");
-    articles += `
-      <article> 
-        <div class="date">
-          ${toDate(issue.closed_at)}
-        </div>
-        <div class="title">  
-          <h1>${issue.title}</h1>
-        </div>
-        <div class="labels">
-          ${labels}
-        </div>
-        <div class="post">
-          ${toHtml(toEntities(issue.body))}
-        </div>
-      </article>
-    `;
-  }
-  render(`
-    <main>
-      ${articles}
-    </main>
-    <footer>
-      <a href="https://twitter.com/perdjurner">
-        <img src="/images/twitter.svg" alt="Twitter logo">
-        <p>${user.name}</p>
-      </a>
-    </footer>
-`);
+  let posts = await api("repos/perdjurner/blog/issues?state=closed");
+
+  // Make sure only posts created by the repository owner are included.
+  posts = posts.filter((elm) => elm.author_association === "OWNER");
+
+  // Get either the post matching the URL hash or sort all posts.
+  const hash = window.location.hash.replace(/[^\-a-z]/g, "");
+  const byClosedAt = (a, b) => (a.closed_at < b.closed_at ? 1 : -1);
+  const onHash = (elm) => hash === elm.title.replace(/ /g, "-").toLowerCase();
+  posts = hash ? posts.filter(onHash) : [...posts].sort(byClosedAt);
+
+  // Build and render HTML.
+  render(
+    `<main>${postsHtml(posts)}</main><footer>${footerHtml(user)}</footer>`
+  );
 })().catch((e) => {
   console.error(e);
 });
