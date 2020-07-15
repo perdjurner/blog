d79230954ce1e7782a015a4588b86e020af4af70
Per Djurner <per.djurner@gmail.com>
2020-07-15 14:08:43 +0200

    Better regex fix.

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ function toHtml(md) {
       .replace(/\[(.*?)\]\((.*?)\)/g, "<a href='$2'>$1</a>")
       .replace(/`(.*?)`/g, "<code>$1</code>")
       .replace(/\*\*(.*)\*\*/g, "<strong>$1</strong>")
-      .replace(/_(.*)_([^a-z])/gi, "<strong>$1</strong>$2")
+      .replace(/([^a-z])_(.*?)_([^a-z])/gi, "$1<strong>$2</strong>$3")
       .replace(/\r\n\r\n/g, "</p><p>")
       .replace(/\r\n/g, "<br>") +
     "</p>"
