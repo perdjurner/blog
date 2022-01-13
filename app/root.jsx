d0d3d2e7b5bce85608e2c639b5dbfd82b819b777
Per Djurner <per.djurner@gmail.com>
2022-01-13 19:34:40 +0100

    Update

--- app/root.jsx
+++ app/root.jsx
@@ -17,7 +17,7 @@ export const links = () => {
   return [
     {
       rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700",
+      href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap",
     },
     {
       rel: "icon",
