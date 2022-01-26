0ac5c6e62bb9eedd6258571aaf715c79f1d074ba
Per Djurner <per.djurner@gmail.com>
2022-01-26 21:21:23 +0100

    Fix onload

--- app/root.jsx
+++ app/root.jsx
@@ -18,7 +18,7 @@ export const links = () => {
     {
       rel: "stylesheet",
       media: "print",
-      onload: "this.onload=null;this.removeAttribute('media');",
+      onLoad: "this.onload=null;this.removeAttribute('media');",
       href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap",
     },
     {
